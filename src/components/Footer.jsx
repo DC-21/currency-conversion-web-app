9bfc8aec1dfcdf5351e0e3c2094416d488be7916
DC-21 <cholahkuboko@gmail.com>
2023-07-12 15:22:57 +0200

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,21 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="py-4 px-3 items-center w-full">
-      <div className="container px-4 py-6 mx-auto rounded-lg flex items-center sm:flex-row flex-col bg-[#260c65]">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <span className="text-white ml-3 text-xl">DiCE</span>
-        </a>
-        <p className="text-sm text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+    <div className="md:py-4 py-0 px-3 items-center w-full">
+      <div className="container py-6 px-4 mx-auto rounded-lg flex items-center sm:flex-row flex-col bg-[#260c65]">
+        <p className="text-sm text-white sm:py-2 sm:mt-0 mt-0">
           © 2023 DC WRLD —
           <a href="https://twitter.com/dcwrld" className="text-white ml-1">
             @dc wrld
