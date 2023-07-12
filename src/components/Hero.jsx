62e0e6033419437f94f4f704b4ad88f7a6565512
DC-21 <cholahkuboko@gmail.com>
2023-07-12 12:19:54 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,11 +7,12 @@ const Hero = () => {
         <p className="text-center text-3xl text-[#260c65]">
           Online Exchange Rates
         </p>
-        <p>
-          The 100% free solution for handling exchange rate conversions. Our
-          currency API provides live & historical exchange rate data ranging
-          from 1999 until today.
+        <p className="text-center text-xl text-[#7560a5] pt-4">
+          The 100% free solution for handling exchange rate conversions.
         </p>
+        <p className="text-center text-xl text-[#7560a5]"> Our
+          currency conversion website provides live & historical exchange rate data ranging
+          from 2000 until today.</p>
       </div>
       <div></div>
     </div>
